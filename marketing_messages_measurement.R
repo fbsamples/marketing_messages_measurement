2edee1131ea2cf2a982c64ef2d825905b33c6124
melinamsk <melinamsk@meta.com>
2023-09-11 17:43:40 -0300

    WhatsApp Marketing Messages ROI Assessment
    The goal of this script is to provide technical detail on a proposal to define how Power Calculation and Data Analysis should be defined when piloting WhatsApp Marketing Messages measurement with clients. This is a new measurement proposal must be executed at the advertiser / partner side, in their environment.
    It's used to compare WhatsApp Marketing Messages performance against other channels such as Email or SMS Marketing.
    
    The method consists of using conversion rates estimates, applying the fixed cost to the amount of impressions and calculate CPA.

--- marketing_messages_measurement.R
+++ marketing_messages_measurement.R
@@ -1,9 +1,30 @@
-# Power analysis
+# This project provides best practices on how to measure WhatsApp marketing
+# messages effectively, understand how many incremental conversions businesses can get 
+# as well as how to compare the effectiveness of marketing messages against other external 
+# platforms such as email/SMS. A brief summary of how these tests are performed is below:
 
-### Scenario 1: INCREMENTALITY ###
+# Define if you want to benchmark WhatsApp marketing messages against other scaled channels 
+# such as email marketing and SMS. We strongly recommend this comparison to help us understand
+# how valuable marketing messages are across channels. The target list of customers should have 
+# the same probability of assignment in each of the cells tested (we call this process randomization) and, 
+# ideally, should be split equally.
+# The easiest way of doing this is assigning them randomly in the following example cells:
+#   1. [Type A] Incrementality of Whatsapp marketing messages - eg: 50% control / 50% WA mkt msgs
+#   2. [Type B] Benchmarks - eg: 50% SMS or email mkt / 50% WA mkt msgs
+
+
+#
+##
+####### POWER CALCULATION #######
+
+# We perform the power calculation analysis to make sure that the experiment has a sufficient audience size 
+# that will allow us to detect differences. An experiment that is not well-powered will likely generate 
+# false negative results for the effect size that we are looking for.
+
+### INCREMENTALITY OF MARKETING MESSAGES (Type A) ###
 # this power calculation is straightforward, as it requires a minimum effect size between two proportions
 # In this type of research, the cost is only applied to the treatment cell, so we can calculate the cost per incremental
-# conversion metric. We only need to power this experiment to be able to detect a minimum difference between conv rates
+# conversion metric. We only need to power this experiment to be able to detect a minimum difference between conversion rates
 # at 80% power, 5% significance level and a minimum lift of 10%
 
 control_cvr = 0.05 # Benchmark from previous WA campigns. Conversion rate = 5%
@@ -18,38 +39,31 @@ power.prop.test(
   alternative = "two.sided"
 )
 
-# --- Output: Two-sample comparison of proportions power calculation
 
-## BENCHMARK OF WA VS LEGACY CHANNELS
-
+### BENCHMARK OF WHATSAPP MARKETING MESSAGES AGAINST OTHER CHANNELS (type B) ###
 # For this design, the power analysis is more complex because the difference in conversion rates doesn't
 # take into account the difference in cost between the alternatives.
 # In this case, we need to estimate a baseline of conversion rates for the control group that equalizes for
 # the difference in costs. By doing so, we will have a well-powered experiment 
-# to be able to detect a specific difference in CPA.
-
+# to be able to detect a specific difference in cost per action (purchases).
 # Eg: Whatsapp vs SMS, and the cost to send a msg in each channel is USD 0.05 and USD 0.02 respectively.
 # from historical data, we know SMS's convertion rate is 1.5%
-
-# cost per message ratio between WA and SMS: 0.05 / 0.02 = 2.5
-
-# The next step is to use the multiplier times the conversion rate of SMS to define a new baseline:
-# 2.5 * 0.015 = 0.0375
-
-# then we proceed with the power calculation for 2 different proportions. The baseline for control is 
+# Then we calculate the cost per message sent ratio between WhatsApp and SMS: 0.05 / 0.02 = 2.5
+# The next step is to use the multiplier times the conversion rate of SMS to define a new baseline: 2.5 * 0.015 = 0.0375
+# Then we proceed with the power calculation for 2 different proportions. The baseline for control is 
 # the number calculated in the step above (0.0375), and the test cell will be the baseline * the minimum
-# expeted lift (let's assume 10%)
+# expected lift (let's assume 10%)
 # Baseline (control): 0.0375
 # Test cell: 0.0375 * 1.10 (10% improvement) = 0.04125
 # Min amount of conversions = 75 is a result of previous experiences at Meta on minimum requirements for power calculation
 
-sms_cost = 0.02
-wa_cost = 0.05
-sms_cvr = 0.015
-wa_cvr = (wa_cost/sms_cost)*sms_cvr
-min_conversions = 75 # minimum amount of conversions
-sms_n = min_conversions / sms_cvr # Min sample size for SMS for power calc
-wa_n = min_conversions / wa_cvr # min sample size for WA for power calc
+sms_cost = 0.02 # cost per sms sent
+wa_cost = 0.05 # cost per whatsapp mkt message sent
+sms_cvr = 0.015 # sms conversion rate 
+wa_cvr = (wa_cost/sms_cost)*sms_cvr # wa conversion rate - accounting for differences in costs
+min_conversions = 75 # minimum amount of conversions 
+sms_n = min_conversions / sms_cvr # Min sample size for SMS for power calculations
+wa_n = min_conversions / wa_cvr # min sample size for WA for power calculations
 
 library(pwr)
 
@@ -100,38 +114,63 @@ pwr.2p2n.test(
 # SMS treatment: 75000 people reached
 # WA treatment: 30000 people reached
 
-##### RANDOMIZATION #######
+
+#
+##
+####### RANDOMIZATION #######
+
 # In this step, you will divide your customers into the treatment groups. If you’re running an
 # incrementality analysis (type A), you will have the control (holdout) group and the test group
 # (will receive WhatsApp messages). If you’re running the benchmark analysis (type B), you will
 # have to randomize the audience in the same way, but in the respective treatment groups
 
-# to test if the randomization was well executed, you should run some comparison tests for several 
+# The assignment should be done randomly to guarantee that the only aspect that differs between the groups is the treatment.
+# To test if the randomization was well executed, you should run some comparison tests for several 
 # metrics for historical behavior:
 # Number of WA messages received in the last 28 days
 # Number of SMS messages received in the last 28 days
 # Number of purchases in the last 28 days
 # age, gender, location
+# You should evaluate if those metrics differ, on average, between groups before executing the test. 
+# This can be done using a t-test for continuous variables or chi-square tests for discrete variables. 
 
 ## checking randomization using package tableone
 library(tableone) # this package helps scaling t-tests for all metrics from your table
 library(dplyr)
-base_data <- read.csv('base_data.csv', header = TRUE, sep = ";")
+base_data <- read.csv('base_data.csv', header = TRUE, sep = ";") # randomized database 
 
 table1 = CreateTableOne(
   vars = c("wa_messages_28d", "email_messages_28d", "purchases_28d"), # all vars from the table
-  strata = "group", # var that indicates the stratification assigment
+  strata = "group", # variables that indicates the stratification assignment
   data = base_data, #data input - from the client
   test = T
 )
 summary(table1)
 
 # --- Output: summary of continuous variables
-# compare the p-value for each variable: we want pvalues > 0.05 for all vars - if pvalue < 0.05, that means 
-# the randomization is skewed or biased in that variable, and we need to re-run it
+# For the list of variables, you should expect that none of them differ statistically significantly at 5%
+# Compare the p-value for each variable: we want pvalues > 0.05 for all vars - if pvalue < 0.05, it could mean that
+# the groups were not very well randomized, so we would recommend to run the randomization again
 
-# ------ ########## DATA ANALYSIS FOR SCENARIO 1: INCREMENTALITY OF WA ########
-# Assuming we run the experiment with the proper power mentioned earlier
+
+#
+##
+####### DATA ANALYSIS #######
+# The data analysis is executed depending on which type of research design was selected. 
+# In summary, each analysis type has a different focus on what metric is compared against control (holdout) and test groups:
+#   1. Incrementality of marketing messages (Type A): for this type of analysis, the focus is on comparing the 
+#   conversion rates between holdout and test group using a proportions test. 
+#   The outcome of this analysis is the conversion rates (CVR) confidence intervals of each group as well as a p-value for the difference in proportions. The test is executed through an analytical solution and is presented below with an example.
+#   2. Benchmark of marketing messages against other channels (Type B): for this type of analysis,
+#   the focus is comparing the cost per action (CPA), where the action is the conversion event, between channels. 
+#   The outcome of this analysis is the CPA confidence intervals of each group as well as a p-value for the difference in CPAs. 
+#   The test is executed through simulations given the experimental data
+
+## --- DATA ANALYSIS FOR INCREMENTALITY OF MARKETING MESSAGES (Type A) --- ##
+# Let’s assume that we used the power calculation example above and we landed an experiment with 32K customers 
+# in both holdout/test groups. The holdout group, where the message was not sent, had 1,430 conversions, 
+# and the test group, where the message was sent, had 1,705 conversions. 
+# Assuming the cost of USD 0.05 per message sent, we have the following code:
 n = 32000 # sample size in each group
 holdout_conversions = 1430 # provided by client / partner
 test_conversions = 1705 # provided by client / partner
@@ -158,11 +197,12 @@ cost_per_incremental_conversions
 
 # ---- Output: 2-sample test for equality of proportions with continuity correction
 
-# ------ ########## DATA ANALYSIS FOR SCENARIO 2: BENCHMARK ANALYSIS ########
+## --- DATA ANALYSIS FOR BENCHMARK OF MARKETING MESSAGES WITH OTHER CHANNELS (Type B) --- ##
 # Assuming the power calculation scenario described above
 # We sent 75k messages through SMS and 35k through WA
 # cost per message sent is 0.02 for SMS and 0.05 for WA
 # conversions: 1490 for SMS and 1678 for WA
+# The code below will run the simulation with 10,000 random conversions given experimental data and calculate results:
 
 # Setting parameters
 sms_n = 75000 # SMS messages sent
@@ -212,4 +252,7 @@ deltaSummary = data.frame(
 rownames(deltaSummary) = c()
 deltaSummary
 
-# when the confidence intervals do not contain zeros, and pvalue < 0.05: Difference is stat sig
+# The two output tables summarize important findings, showing a delta of CPAs between WhatsApp and SMS
+# If the the confidence intervals for delta don’t contain zero, and p-value is <0.05, it indicates that this difference 
+# is statistically significant. 
+# Using charting, you can plot the confidence intervals and averages for proper visualization.
